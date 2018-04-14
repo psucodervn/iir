336fd098a22476bbf95c6d0464f9a7f6c37eebb1
Hung Le <hungle.info@gmail.com>
2018-04-14 21:37:49 +0700

    Add test cases

--- cmd/parser.go
+++ cmd/parser.go
@@ -1,9 +1,18 @@
 package cmd
 
-// TestCase interface
-type TestCase interface {
-	Input() string
-	Output() string
+// TestCase struct
+type TestCase struct {
+	Input  string `json:"input"`
+	Output string `json:"output"`
+}
+
+// NewTestCase create new TestCase with input and output
+func NewTestCase(input, output string) *TestCase {
+	tc := &TestCase{
+		Input:  input,
+		Output: output,
+	}
+	return tc
 }
 
 // Task interface
@@ -11,6 +20,65 @@ type Task interface {
 	Name() string
 	Description() string
 	TestCases() []TestCase
-	TimeLimit() int
-	MemoryLimit() int
+	TimeLimit() int64
+	MemoryLimit() int64
+}
+
+// DefaultTask is the default implement of Task interface
+type DefaultTask struct {
+	name        string     `json:"name"`
+	description string     `json:"description"`
+	testCases   []TestCase `json:"testCases"`
+	timeLimit   int64      `json:"timeLimit"`
+	memoryLimit int64      `json:"memoryLimit"`
+}
+
+// SetName set task name
+func (task *DefaultTask) SetName(name string) {
+	task.name = name
+}
+
+// SetDescription set task description
+func (task *DefaultTask) SetDescription(description string) {
+	task.description = description
+}
+
+// SetTestCases set task test cases
+func (task *DefaultTask) SetTestCases(testCases []TestCase) {
+	task.testCases = testCases
+}
+
+//SetTimeLimit set task time limit
+func (task *DefaultTask) SetTimeLimit(timeLimit int64) {
+	task.timeLimit = timeLimit
+}
+
+//SetMemoryLimit set task memory limit
+func (task *DefaultTask) SetMemoryLimit(memoryLimit int64) {
+	task.memoryLimit = memoryLimit
+}
+
+// Name return task name
+func (task *DefaultTask) Name() string {
+	return task.name
+}
+
+// Description return task description
+func (task *DefaultTask) Description() string {
+	panic("implement me")
+}
+
+// TestCases return all test case
+func (task *DefaultTask) TestCases() []TestCase {
+	return task.testCases
+}
+
+// TimeLimit return time limit in nanosecond unit
+func (task *DefaultTask) TimeLimit() int64 {
+	return task.timeLimit
+}
+
+// MemoryLimit return memory limit in Byte unit
+func (task *DefaultTask) MemoryLimit() int64 {
+	return task.memoryLimit
 }
--- cmd/parser_test.go
+++ cmd/parser_test.go
@@ -1,1 +1,364 @@
 package cmd
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTestCase(t *testing.T) {
+	as := assert.New(t)
+
+	tc := NewTestCase("input", "output")
+	as.NotNil(tc)
+	as.Equal(tc.Input, "input")
+	as.Equal(tc.Output, "output")
+}
+
+func TestDefaultTaskImplementsTask(t *testing.T) {
+	as := assert.New(t)
+
+	as.Implements((*Task)(nil), new(DefaultTask))
+}
+
+func TestDefaultTask_SetName(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{{
+		name:   "set name to Task1",
+		fields: fields{name: "Task1"},
+		args:   args{name: "Task1"},
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			task.SetName(tt.args.name)
+		})
+	}
+}
+
+func TestDefaultTask_SetDescription(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	type args struct {
+		description string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			task.SetDescription(tt.args.description)
+		})
+	}
+}
+
+func TestDefaultTask_SetTestCases(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	type args struct {
+		testCases []TestCase
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			task.SetTestCases(tt.args.testCases)
+		})
+	}
+}
+
+func TestDefaultTask_SetTimeLimit(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	type args struct {
+		timeLimit int64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			task.SetTimeLimit(tt.args.timeLimit)
+		})
+	}
+}
+
+func TestDefaultTask_SetMemoryLimit(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	type args struct {
+		memoryLimit int64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			task.SetMemoryLimit(tt.args.memoryLimit)
+		})
+	}
+}
+
+func TestDefaultTask_Name(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{{
+		name:   "task name should be `Task1`",
+		fields: fields{name: "Task1"},
+		want:   "Task1",
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			if got := task.Name(); got != tt.want {
+				t.Errorf("DefaultTask.Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDefaultTask_Description(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			if got := task.Description(); got != tt.want {
+				t.Errorf("DefaultTask.Description() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCases(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	testCases := []TestCase{
+		TestCase{},
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []TestCase
+	}{{
+		name: "set testCases to empty",
+		fields: fields{
+			testCases: []TestCase{},
+		},
+		want: []TestCase{},
+	}, {
+		name: "set testCases to an array",
+		fields: fields{
+			testCases: testCases,
+		},
+		want: testCases,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			if got := task.TestCases(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DefaultTask.TestCases() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDefaultTask_TimeLimit(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int64
+	}{{
+		name: "set timeLimit to 1s",
+		fields: fields{
+			timeLimit: 1000000,
+		},
+		want: 1000000,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			if got := task.TimeLimit(); got != tt.want {
+				t.Errorf("DefaultTask.TimeLimit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDefaultTask_MemoryLimit(t *testing.T) {
+	type fields struct {
+		name        string
+		description string
+		testCases   []TestCase
+		timeLimit   int64
+		memoryLimit int64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int64
+	}{{
+		"1000 mem",
+		fields{
+			memoryLimit: 1000,
+		},
+		1000,
+	},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &DefaultTask{
+				name:        tt.fields.name,
+				description: tt.fields.description,
+				testCases:   tt.fields.testCases,
+				timeLimit:   tt.fields.timeLimit,
+				memoryLimit: tt.fields.memoryLimit,
+			}
+			if got := task.MemoryLimit(); got != tt.want {
+				t.Errorf("DefaultTask.MemoryLimit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
